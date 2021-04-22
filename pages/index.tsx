018e3577132c0f17cdb43b4c964a34d79c7fe336
danjudex <dan.judex@gmail.com>
2021-04-22 16:47:00 +0000

    blog

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,33 @@
-import { Board } from "../components/board/Board";
-import { boards } from "../data/boards";
+import { useEffect, useState } from "react";
+
+import { Card } from "../components/album/Card";
 import { DefaultLayout } from "../layouts/DefaultLayout";
 
 const HomePage = () => {
+  const [users, setUsers] = useState([]);
+
+  const fetchUsers = async () => {
+    const r = await fetch("/api/users");
+    const users = await r.json();
+
+    setUsers(users);
+  };
+
+  useEffect(() => {
+    fetchUsers();
+  }, []);
+
   return (
     <DefaultLayout>
-      {boards.map((i, index) => {
-        return <Board key={index} {...i} />;
-      })}
+      <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+        {users.map((i, index) => {
+          return (
+            <div key={index} className="col">
+              <Card {...i} />
+            </div>
+          );
+        })}
+      </div>
     </DefaultLayout>
   );
 };
